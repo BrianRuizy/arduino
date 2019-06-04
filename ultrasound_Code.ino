e534a58016ee9b28eefa814e073b8ef1c4769652
Brian Ruiz <brianruiz123@yahoo.com>
2019-06-04 12:52:01 -0500

    Add files via upload

---
+++ ultrasound_Code.ino
@@ -0,0 +1,23 @@
+int redPin= 5;
+int greenPin = 6;
+int bluePin = 9;
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+void loop() {
+  setColor(255,0, 0); // Red Color
+  delay(500);
+  
+  setColor(0, 0, 255); // Blue Color
+  delay(500);
+  
+   setColor(0, 255, 0); // green Color
+  delay(500);
+}
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
